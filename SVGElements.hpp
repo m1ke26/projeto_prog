4be0fbfdc9e95909e37203b14db739fd929d540c
m1ke26 <msm.mimoso@gmail.com>
2026-06-02 17:27:39 +0100

    Initial commit

---
+++ SVGElements.hpp
@@ -0,0 +1,65 @@
+//! @file shape.hpp
+#ifndef __svg_SVGElements_hpp__
+#define __svg_SVGElements_hpp__
+
+#include "Color.hpp"
+#include "Point.hpp"
+#include "PNGImage.hpp"
+
+namespace svg
+{
+    class SVGElement
+    {
+
+    public:
+        SVGElement();
+        virtual ~SVGElement();
+        virtual void draw(PNGImage &img) const = 0;
+    };
+
+    // Declaration of namespace functions
+    // readSVG -> implement it in readSVG.cpp
+    // convert -> already given (DO NOT CHANGE) in convert.cpp
+
+    void readSVG(const std::string &svg_file,
+                 Point &dimensions,
+                 std::vector<SVGElement *> &svg_elements);
+    void convert(const std::string &svg_file,
+                 const std::string &png_file);
+
+    class Ellipse : public SVGElement
+    {
+    public:
+        Ellipse(const Color &fill, const Point &center, const Point &radius);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color fill;
+        Point center;
+        Point radius;
+    };
+
+    class Circle : public Ellipse
+    {
+    public:
+        Circle(const Color &fill, const Point &center, const int &r);
+    };
+
+    class Polyline : public SVGElement
+    {
+    public:
+        Polyline(const Color &stroke, std::vector <Point> &points);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color stroke;
+        std::vector <Point> points;
+    };
+
+    class Line : public Polyline
+    {
+    public:
+        Line(const Color &stroke, std::vector <Point> &points);
+    };
+}
+#endif
